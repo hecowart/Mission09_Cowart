3d4f1e4f591e1b423da07a0e4b9f98bc6064c51c
hecowart <haydenecowart@gmail.com>
2024-03-08 23:11:26 -0700

    Completed my assignment of Mission 9 for IS 413. It is an app which imports data from an external JSON file and displays each JSON object's info on a card. The data is College Basketball teams.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './CollegeBasketballTeams.json';
+
+//create variable to reference down in our 'for loop'
+const teamList = data.teams;
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return (
+    <div>
+      <h1>NCAA Basketball</h1>
+      <h2>
+        With March upon us, let's take a look at all of the colleges in NCAA
+        Basketball!
+      </h2>
+      <br />
+    </div>
+  );
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div className="team-card">
+        <h2>{oneTeam.school}</h2>
+        <h4>Mascot Name: {oneTeam.name}</h4>
+        <h4>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h4>
+        <br />
+      </div>
+    );
+  }
+}
+
+export { Team };
+
+function TeamList() {
+  return (
+    //this is like a for loop to iterate through the Json objects
+    <div className="team-list">
+      {teamList.map((oneTeam) => (
+        <Team {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
-
   //const blah = "hi";
   //we need to install something called eslint which will enforce rule that unused variables aren't cool
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
