b5c55f1293d8b59659553ee926b5a1e109b9df32
hecowart <haydenecowart@gmail.com>
2024-03-08 19:47:38 -0700

    I set up the default setup of React

---
+++ blah/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  //const blah = "hi";
+  //we need to install something called eslint which will enforce rule that unused variables aren't cool
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
